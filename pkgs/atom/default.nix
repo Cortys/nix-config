53033f56bb1745800b274dda7597fb55ed8921aa
Clemens Damke <admin@cortys.de>
2016-11-10 23:53:21 +0100

    transformed atom drv into a simple diff definition to merge into nixpkgs

--- pkgs/atom/default.nix
+++ pkgs/atom/default.nix
@@ -1,6 +1,4 @@
-{ stdenv, fetchurl, lib, makeWrapper, gvfs, atomEnv }:
-
-stdenv.mkDerivation rec {
+rec {
   name = "atom-${version}";
   version = "1.11.2";
 
@@ -8,38 +6,5 @@ stdenv.mkDerivation rec {
     url = "https://github.com/atom/atom/releases/download/v${version}/atom-amd64.deb";
     sha256 = "1mvlj1j0hyvm5di95nn0x99lm5arw2amm1s1va1m73zss3bzlhpm";
     name = "${name}.deb";
-  };
-
-  nativeBuildInputs = [ makeWrapper ];
-
-  buildCommand = ''
-    mkdir -p $out/usr/
-    ar p $src data.tar.gz | tar -C $out -xz ./usr
-    substituteInPlace $out/usr/share/applications/atom.desktop \
-      --replace /usr/share/atom $out/bin
-    mv $out/usr/* $out/
-    rm -r $out/share/lintian
-    rm -r $out/usr/
-    wrapProgram $out/bin/atom \
-      --prefix "PATH" : "${gvfs}/bin"
-
-    fixupPhase
-
-    patchelf --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" \
-      --set-rpath "${atomEnv.libPath}:$out/share/atom" \
-      $out/share/atom/atom
-    patchelf --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" \
-      --set-rpath "${atomEnv.libPath}" \
-      $out/share/atom/resources/app/apm/bin/node
-    patchelf --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" \
-      $out/share/atom/resources/app.asar.unpacked/node_modules/symbols-view/vendor/ctags-linux
-  '';
-
-  meta = with stdenv.lib; {
-    description = "A hackable text editor for the 21st Century";
-    homepage = https://atom.io/;
-    license = licenses.mit;
-    maintainers = [ maintainers.offline maintainers.nequissimus ];
-    platforms = [ "x86_64-linux" ];
   };
 }
